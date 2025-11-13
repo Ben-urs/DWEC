67fe3d3c8e86e67c63e733e810f8cf7173326bd9
BenCP <beniaminpatras@gmail.com>
2025-11-13 22:28:04 +0100

    Finalizado

---
+++ UD 2/AC 2/blogAngular/src/app/components/navbar_comp/navbar-comp.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-component',
+  imports: [],
+  templateUrl: './navbar-comp.html',
+  styleUrl: './navbar-comp.css',
+})
+
+export class NavbarComponent {
+
+}
